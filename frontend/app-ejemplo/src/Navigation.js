5b5f928a9eebe7423d782d6ce74b49a0900bff3c
Samito9920 <sajara.dwfsc13@bootcamp.ucamp.io>
2023-06-20 23:35:08 -0500

    front

---
+++ frontend/app-ejemplo/src/Navigation.js
@@ -0,0 +1,37 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import React from "react";
+import { Text, View } from "react-native";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import Menu from "./components/home/Menu";
+import { StyleSheet } from "react-native";
+
+
+const tab = createBottomTabNavigator();
+
+const Navigation = () => {
+    return (
+        <tab.Navigator>
+            <tab.Screen name="Home" component={Menu} options={{
+                tabBarLabel: "Home",
+                tabBarIcon: ({ color, size }) => {
+                    <MaterialCommunityIcons name="home" color={color} size={size} />
+                }
+            }}>
+
+            </tab.Screen>
+
+        </tab.Navigator>
+    )
+
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: 'blue',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+
+export default Navigation;
