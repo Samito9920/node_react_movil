14faa747dc5104a3fd64dd17b83c1c25af0eb9bc
Samito9920 <116050181+Samito9920@users.noreply.github.com>
2023-06-30 12:32:46 +0000

    primera aprte

--- frontend/app-ejemplo/src/components/home/ProfileCard.js
+++ frontend/app-ejemplo/src/components/home/ProfileCard.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { StyleSheet, View, Image, Text, Linking } from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
+import dog from '../assets/dogs.png'
 
 
 const Google = <Icon  name="google" size={30} color="black" />
@@ -8,19 +9,18 @@ const Google = <Icon  name="google" size={30} color="black" />
 const Facebook = <Icon  name="facebook" size={30} color="black" />
 const Instagram = <Icon  name="instagram" size={30} color="black" />
 const Twitter = <Icon  name="twitter" size={30} color="black" />
-
-
+const TikTok = <Icon name="tiktok" size={30} color="black"/>
 
 
 const ProfileCard = () => {
     const user = {
-        avatar :"https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80",
+        avatar :"https://grupoinmotion.com/10-formas-de-aprovechar-la-inteligencia-artificial-ia-y-brindar-experiencias-del-cliente-excepcionales/",
         coverphoto: "https://wallpapers.com/images/hd/profile-picture-background-10tprnkqwqif4lyv.jpg",
         name:"Samantha Jara"
     }
     return(
         <View style={styles.container}>
-            <Image source={{ uri: user.coverphoto }} style={styles.coverPhoto} />
+            <Image source={dog} style={styles.coverPhoto} />
             <View style={styles.avatarContainer}>
                 <Image source={{ uri: user.avatar }} style={styles.avatar} />
                 <Text style={styles.name}>
@@ -40,6 +40,7 @@ const ProfileCard = () => {
                 <Text style={{ color: "blue" }} onPress={() => Linking.openURL("https://www.google.com/")}>
                     {Twitter}
                 </Text>
+                
             </View>
         </View>
     )
